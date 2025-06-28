bb264b93de59d026bbb84c9da8eed0a2651c871c
punamsomvanshi <punamsomvanshi29@gmail.com>
2025-06-28 12:20:40 +0530

    first commit

--- src/main/java/CodingChallenges/challenge_1/Challenge1.java
+++ src/main/java/CodingChallenges/challenge_1/Challenge1.java
@@ -3,7 +3,14 @@ package CodingChallenges.challenge_1;
 public class Challenge1
 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+      String s = "Hello World";
+        System.out.println("Lengths : " + s.length());
+
+        System.out.println("First char : "+ s.charAt(0));
+
+        String s1 = s.substring(5,11);
+        System.out.println("substring : "+ s1);
+
     }
 
 }
